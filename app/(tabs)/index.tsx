a3078f4013af035b4f4bc320a9bf8df2fafed491
eckofox1981 <eckofox1981@pm.me>
2025-10-03 14:55:33 +0200

    feat: add CurrentWeather

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,15 @@
+import { CurrentWeather } from "@/components/currentWeather";
 import { GpsSearch } from "@/components/gpsSearch";
 import { SearchBar } from "@/components/searchBar";
-import { ThemedText } from "@/components/themed-text";
-import { StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={styles.main}>
+    <View style={styles.main}>
       <SearchBar />
       <GpsSearch />
-      <ThemedText>MAIN</ThemedText>
-    </SafeAreaView>
+      <CurrentWeather />
+    </View>
   );
 }
 
---
+++ components/currentWeather.tsx
@@ -0,0 +1,73 @@
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import { ThemedIcon } from "./themed-icon";
+import { ThemedText } from "./themed-text";
+
+export function CurrentWeather() {
+  return (
+    <View style={styles.main}>
+      <ThemedText style={styles.cityName}>Älmhult, Sweden</ThemedText>
+      <View style={styles.weatherContainer}>
+        <View style={styles.leftContainer}>
+          <Image
+            style={{ flex: 1, resizeMode: "contain" }}
+            source={{
+              uri: "https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather03-256.png",
+            }}
+          />
+          <TouchableOpacity style={styles.bookmark}>
+            <ThemedIcon size={40} name="heart.fill" />
+            <ThemedText style={styles.bookmarkText}>
+              Bookmark{"\n"}city
+            </ThemedText>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.rightContainer}>
+          <ThemedText>Temp: 27*C</ThemedText>
+          <ThemedText>Feels like 27*C</ThemedText>
+          <ThemedText>Clear sky</ThemedText>
+          <ThemedText>Gentle breeze</ThemedText>
+          <ThemedText>5.1 m/s ENE</ThemedText>
+          <ThemedText>1017 hPa</ThemedText>
+          <ThemedText>Humdity: 38%</ThemedText>
+          <ThemedText>UV: 3</ThemedText>
+          <ThemedText>Dew point: 12*C</ThemedText>
+          <ThemedText>Visibility: 10 km</ThemedText>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  main: {
+    alignItems: "center",
+    width: "95%",
+    padding: 10,
+    borderBottomColor: "#3300FF",
+    borderBottomWidth: 2,
+  },
+  weatherContainer: {
+    flexDirection: "row",
+    gap: 5,
+  },
+  cityName: {
+    fontWeight: 600,
+    fontSize: 20,
+  },
+  leftContainer: {
+    width: "50%",
+  },
+  rightContainer: {
+    width: "50%",
+    paddingLeft: 10,
+  },
+  bookmark: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+    justifyContent: "flex-start",
+  },
+  bookmarkText: {
+    flex: 1,
+  },
+});
