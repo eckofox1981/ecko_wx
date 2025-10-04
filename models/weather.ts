1003fe0a62b9d5ff20ffdb37f0012840c188d24b
eckofox1981 <eckofox1981@pm.me>
2025-10-04 11:11:18 +0200

    feat: add HourlyForecast

--- models/weather.ts
+++ models/weather.ts
@@ -12,6 +12,7 @@ export class CurrentWeather {
   visibility: number;
   wind_speed: number;
   wind_deg: number;
+  wind_gust: number;
   mainWeather: string;
   descriptionWeather: string;
   iconWeather: string;
@@ -30,6 +31,7 @@ export class CurrentWeather {
     visibility: number,
     wind_speed: number,
     wind_deg: number,
+    wind_gust : number;
     mainWeather: string,
     descriptionWeather: string,
     iconWeather: string
@@ -47,6 +49,59 @@ export class CurrentWeather {
     this.visibility = visibility;
     this.wind_speed = wind_speed;
     this.wind_deg = wind_deg;
+    this.wind_gust = wind_gust;
+    this.mainWeather = mainWeather;
+    this.descriptionWeather = descriptionWeather;
+    this.iconWeather = iconWeather;
+  }
+}
+
+export class HourlyForeCast {
+  dt: Date;
+  temp: number;
+  feels_like: number;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  uvi: number;
+  clouds: number;
+  visibility: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust : number;
+  mainWeather: string;
+  descriptionWeather: string;
+  iconWeather: string;
+
+  constructor(
+    dt: Date,
+    temp: number,
+    feels_like: number,
+    pressure: number,
+    humidity: number,
+    dew_point: number,
+    uvi: number,
+    clouds: number,
+    visibility: number,
+    wind_speed: number,
+    wind_deg: number,
+    wind_gust : number,
+    mainWeather: string,
+    descriptionWeather: string,
+    iconWeather: string
+  ) {
+    this.dt = dt;
+    this.temp = temp;
+    this.feels_like = feels_like;
+    this.pressure = pressure;
+    this.humidity = humidity;
+    this.dew_point = dew_point;
+    this.uvi = uvi;
+    this.clouds = clouds;
+    this.visibility = visibility;
+    this.wind_speed = wind_speed;
+    this.wind_deg = wind_deg;
+    this.wind_gust = wind_gust;
     this.mainWeather = mainWeather;
     this.descriptionWeather = descriptionWeather;
     this.iconWeather = iconWeather;
