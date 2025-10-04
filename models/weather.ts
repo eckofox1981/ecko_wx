d30a68d5afce6f1a6d73a792010ea3c3ae21d572
eckofox1981 <eckofox1981@pm.me>
2025-10-04 11:24:47 +0200

    feat: add DailyForecast

--- models/weather.ts
+++ models/weather.ts
@@ -107,3 +107,83 @@ export class HourlyForeCast {
     this.iconWeather = iconWeather;
   }
 }
+
+export class DailyForecast {
+    dt : Date;
+    sunrise : Date;
+    sunset : Date;
+    moon_phase: number;
+    summary: string;
+    tempDay: number;
+    tempMin: number;
+    tempMax: number;
+    tempNight : number;
+    tempEve : number;
+    tempMorn: number;
+    pressure: number;
+    humidity: number;
+    dew_point: number;
+    wind_speed: number;
+    wind_deg: number;
+    wind_gust: number;
+    descriptionWeather : string;
+    iconWeather: string;
+    clouds: number;
+    pop: number; //probability of precipitation
+    rain: number;
+    snow: number; //where available (in mm)
+    uvi: number;
+
+    constructor(
+        dt : Date,
+    sunrise : Date,
+    sunset : Date,
+    moon_phase: number,
+    summary: string,
+    tempDay: number,
+    tempMin: number,
+    tempMax: number,
+    tempNight : number,
+    tempEve : number,
+    tempMorn: number,
+    pressure: number,
+    humidity: number,
+    dew_point: number,
+    wind_speed: number,
+    wind_deg: number,
+    wind_gust: number,
+    descriptionWeather : string,
+    iconWeather: string,
+    clouds: number,
+    pop: number, 
+    rain: number,
+    snow: number, 
+    uvi: number,
+    ) {
+        this.dt = dt;
+    this.sunrise = sunrise;
+    this.sunset = sunset;
+    this.moon_phase = moon_phase;
+    this.summary = summary;
+    this.tempDay  = tempDay;
+    this.tempMin = tempMin;
+    this.tempMax = tempMax;
+    this.tempNight = tempNight;
+    this.tempEve = tempEve;
+    this.tempMorn = tempMorn;
+    this.pressure = pressure;
+    this.humidity = humidity;
+    this.dew_point = dew_point;
+    this.wind_speed = wind_speed;
+    this.wind_deg = wind_deg;
+    this.wind_gust = wind_gust;
+    this.descriptionWeather = descriptionWeather;
+    this.iconWeather = iconWeather;
+    this.clouds = clouds;
+    this.pop = pop; 
+    this.rain = rain;
+    this.snow = snow; 
+    this.uvi = uvi;       
+    }
+
+}
