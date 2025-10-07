e9c457747b4b22b7166717608ab17a18fdee617c
eckofox1981 <eckofox1981@pm.me>
2025-10-07 21:27:22 +0200

    feat: add setCity based on GPS location

--- components/gpsSearch.tsx
+++ components/gpsSearch.tsx
@@ -1,13 +1,45 @@
+import { getCityByCoordinates } from "@/api/getCities";
+import { City } from "@/models/city";
+import { useMainCityStore } from "@/store/cityStore";
 import { useLanguageStore } from "@/store/languageStore";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import * as Location from "expo-location";
+import { useState } from "react";
+import { Alert, StyleSheet, TouchableOpacity, View } from "react-native";
 import { ThemedIcon } from "./themed-icon";
 import { ThemedText } from "./themed-text";
 
 export function GpsSearch() {
   const language = useLanguageStore((store) => store.language);
+  const [status, requestPermission] = Location.useForegroundPermissions();
+  const [errorMsg, setErrorMsg] = useState("");
+  const setCity = useMainCityStore((store) => store.setMainCity);
+
+  const handleGPSPress = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg(language.youDeniedLocationPermission);
+      return;
+    }
+
+    const location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.Low,
+    });
+
+    try {
+      const locationCity: City = await getCityByCoordinates(
+        location.coords.latitude,
+        location.coords.longitude
+      );
+
+      setCity(locationCity);
+    } catch (error: any) {
+      Alert.alert("Error setting city: ", error.message);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleGPSPress}>
         <ThemedIcon size={40} name={"gps-fixed.fill"} />
       </TouchableOpacity>
       <ThemedText style={styles.text}>{language.getGPSlocation}</ThemedText>
