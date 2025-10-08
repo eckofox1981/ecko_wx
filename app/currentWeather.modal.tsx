acc63f0a9b399217efa3810a38894d5d0116b024
eckofox1981 <eckofox1981@pm.me>
2025-10-08 10:52:52 +0200

    feat: add Forecast button and ScrollView

--- app/currentWeather.modal.tsx
+++ app/currentWeather.modal.tsx
@@ -5,7 +5,7 @@ import { useMainCityStore } from "@/store/cityStore";
 import { useLanguageStore } from "@/store/languageStore";
 import { useCurrentWeatherStore } from "@/store/weatherStore";
 import { timeFormating } from "@/utilities/timeFormating";
-import { Image, View } from "react-native";
+import { Image, ScrollView, TouchableOpacity, View } from "react-native";
 
 export default function CurrentWeatherModal() {
   const city = useMainCityStore((store) => store.mainCity);
@@ -16,73 +16,78 @@ export default function CurrentWeatherModal() {
 
   return (
     <View>
-      <ThemedText>Current weather in {city.name}</ThemedText>
+      <View>
+        <ThemedText>Current weather in {city.name}</ThemedText>
+        <TouchableOpacity>Forecast</TouchableOpacity>
+      </View>
       <ThemedText>
         Sunrise: {timeFormating(currentWeather.sysSunrise)} Sunset:
         {timeFormating(currentWeather.sysSunset)}
       </ThemedText>
-      <View>
-        <Image
-          style={{
-            resizeMode: "contain",
-            height: 150,
-            width: 150,
-            backgroundColor: "#3300FF",
-            margin: "auto",
-            borderRadius: 75,
-          }}
-          source={{
-            uri: GET_WEATHER_ICON_URL(currentWeather.weatherIcon),
-          }}
-        />
-        <ThemedText>{currentWeather.weatherDescription}</ThemedText>
-        <ThemedText>
-          {language.visibility}: {currentWeather.visibility}
-        </ThemedText>
-      </View>
-      <View>
-        <ThemedText>{language.temperatures}</ThemedText>
-        <ThemedText>
-          {language.average}: {currentWeather.mainTemp}°C
-        </ThemedText>
-        <ThemedText>
-          {language.minimum}: {currentWeather.mainTemp_min}°C
-        </ThemedText>
-        <ThemedText>
-          {language.maximum}: {currentWeather.mainTemp_max}°C
-        </ThemedText>
-      </View>
-      <View>
-        <ThemedText>{language.wind}</ThemedText>
+      <ScrollView>
         <View>
+          <Image
+            style={{
+              resizeMode: "contain",
+              height: 150,
+              width: 150,
+              backgroundColor: "#3300FF",
+              margin: "auto",
+              borderRadius: 75,
+            }}
+            source={{
+              uri: GET_WEATHER_ICON_URL(currentWeather.weatherIcon),
+            }}
+          />
+          <ThemedText>{currentWeather.weatherDescription}</ThemedText>
+          <ThemedText>
+            {language.visibility}: {currentWeather.visibility}
+          </ThemedText>
+        </View>
+        <View>
+          <ThemedText>{language.temperatures}</ThemedText>
+          <ThemedText>
+            {language.average}: {currentWeather.mainTemp}°C
+          </ThemedText>
+          <ThemedText>
+            {language.minimum}: {currentWeather.mainTemp_min}°C
+          </ThemedText>
+          <ThemedText>
+            {language.maximum}: {currentWeather.mainTemp_max}°C
+          </ThemedText>
+        </View>
+        <View>
+          <ThemedText>{language.wind}</ThemedText>
           <View>
-            <WindCompass degree={currentWeather.windDeg} size={50} />
-            <ThemedText>
-              {language.direction}: {currentWeather.windDeg}°
-            </ThemedText>
-          </View>
-          <View>
-            <ThemedText>
-              {language.speed}: {currentWeather.windSpeed} m/s
-            </ThemedText>
-            <ThemedText>
-              {language.gust}: {currentWeather.windGust} m/s
-            </ThemedText>
-            <ThemedText>
-              {language.pressure}: {currentWeather.mainPressure} hPa
-            </ThemedText>
+            <View>
+              <WindCompass degree={currentWeather.windDeg} size={50} />
+              <ThemedText>
+                {language.direction}: {currentWeather.windDeg}°
+              </ThemedText>
+            </View>
+            <View>
+              <ThemedText>
+                {language.speed}: {currentWeather.windSpeed} m/s
+              </ThemedText>
+              <ThemedText>
+                {language.gust}: {currentWeather.windGust} m/s
+              </ThemedText>
+              <ThemedText>
+                {language.pressure}: {currentWeather.mainPressure} hPa
+              </ThemedText>
+            </View>
           </View>
         </View>
-      </View>
-      <View>
-        <ThemedText>{language.precipitations}</ThemedText>
-        <ThemedText>
-          {language.rainLastHour}: {currentWeather?.rain1h || 0}mm
-        </ThemedText>
-        <ThemedText>
-          {language.humidity}: {currentWeather.mainHumidity}%
-        </ThemedText>
-      </View>
+        <View>
+          <ThemedText>{language.precipitations}</ThemedText>
+          <ThemedText>
+            {language.rainLastHour}: {currentWeather?.rain1h || 0}mm
+          </ThemedText>
+          <ThemedText>
+            {language.humidity}: {currentWeather.mainHumidity}%
+          </ThemedText>
+        </View>
+      </ScrollView>
     </View>
   );
 }
