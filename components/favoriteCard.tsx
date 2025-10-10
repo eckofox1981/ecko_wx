5e6536da14d24bf854df707c6182ac478089c190
eckofox1981 <eckofox1981@pm.me>
2025-10-10 11:16:58 +0200

    feat: add navigation to selected city

--- components/favoriteCard.tsx
+++ components/favoriteCard.tsx
@@ -1,11 +1,20 @@
 import { City } from "@/models/city";
+import { useMainCityStore } from "@/store/cityStore";
 import { cityNameFormating } from "@/utilities/cityNameFormating";
+import { router } from "expo-router";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { ThemedText } from "./themed-text";
 
 export function FavoriteCard({ city }: { city: City }) {
+  const setCity = useMainCityStore((store) => store.setMainCity);
+
+  const goToTown = () => {
+    setCity(city);
+    router.push("/");
+  };
+
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={goToTown}>
       <ThemedText style={styles.name}>{cityNameFormating(city)}</ThemedText>
       <ThemedText style={styles.coordinate}>Lat.: {city.lat}</ThemedText>
       <ThemedText style={styles.coordinate}>Long.: {city.lon}</ThemedText>
@@ -18,11 +27,13 @@ const styles = StyleSheet.create({
     marginVertical: 5,
     borderColor: "#3300FF",
     borderWidth: 2,
+    borderRadius: 15,
     padding: 5,
   },
   name: {
     fontWeight: 600,
     alignSelf: "flex-start",
+    marginLeft: 10,
   },
   coordinate: {
     fontStyle: "italic",
