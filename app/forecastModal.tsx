af68565710fbeafe6ba984be6c2d62b143668f74
eckofox1981 <eckofox1981@pm.me>
2025-10-13 10:50:31 +0200

    fix: fix virtualized issue when combining ScrollView and FlatList

--- app/forecastModal.tsx
+++ app/forecastModal.tsx
@@ -1,19 +1,17 @@
 import { ForecastDetails } from "@/components/forecastDetails";
 import { ThemedText } from "@/components/themed-text";
 import { useForecastStore } from "@/store/weatherStore";
-import { FlatList, ScrollView } from "react-native";
+import { FlatList } from "react-native";
 
 export default function ForecastModal() {
   const forecast = useForecastStore((store) => store.forecast);
 
   return (
-    <ScrollView>
-      <FlatList
-        data={forecast}
-        keyExtractor={(item, index) => `${item.dt} ${index}`}
-        renderItem={({ item }) => <ForecastDetails forecast={item} />}
-        ListEmptyComponent={<ThemedText>No forecast to display</ThemedText>}
-      />
-    </ScrollView>
+    <FlatList
+      data={forecast}
+      keyExtractor={(item, index) => `${item.dt} ${index}`}
+      renderItem={({ item }) => <ForecastDetails forecast={item} />}
+      ListEmptyComponent={<ThemedText>No forecast to display</ThemedText>}
+    />
   );
 }
