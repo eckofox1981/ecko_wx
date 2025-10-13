1710ec60d3cb363ea1de4fbf743bf722c7571149
eckofox1981 <eckofox1981@pm.me>
2025-10-13 13:26:13 +0200

    feat: add color styling of chosen language and temperature units

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -9,6 +9,7 @@ import { Button, StyleSheet, View } from "react-native";
 export default function Settings() {
   const setLanguage = useLanguageStore((store) => store.setLanguage);
   const language = useLanguageStore((store) => store.language);
+  const tempUnit = useTempUnitStore((store) => store.tempUnit);
   const setTempUnit = useTempUnitStore((store) => store.setTempUnit);
   const englishSelected: Language = english;
   const frenchSelected: Language = french;
@@ -24,21 +25,13 @@ export default function Settings() {
     await AsyncStorage.setItem("tempUnit", tempUnit);
   };
 
-  return (
-    <View style={styles.main}>
-      <ThemedText style={styles.title}>{language.settings}</ThemedText>
-      <View style={styles.selection}>
-        <ThemedText style={styles.text}>{language.temperatures}: </ThemedText>
-        <View style={{ gap: 5 }}>
-          <Button title="Celsius" onPress={() => saveTempUnit("celsius")} />
-          <Button title="Farenheit" onPress={() => saveTempUnit("farenheit")} />
-        </View>
-      </View>
-      <View style={styles.selection}>
-        <ThemedText style={styles.text}>{language.language}: </ThemedText>
+  const LanguageButtons = () => {
+    if (language.id === "en") {
+      return (
         <View style={{ gap: 5 }}>
           <Button
             title="English"
+            color={"orange"}
             onPress={() => {
               languageSelection(englishSelected);
             }}
@@ -56,6 +49,98 @@ export default function Settings() {
             }}
           />
         </View>
+      );
+    }
+    if (language.id === "se") {
+      return (
+        <View style={{ gap: 5 }}>
+          <Button
+            title="English"
+            onPress={() => {
+              languageSelection(englishSelected);
+            }}
+          />
+          <Button
+            title="Français"
+            onPress={() => {
+              languageSelection(frenchSelected);
+            }}
+          />
+          <Button
+            title="Svenska"
+            color={"orange"}
+            onPress={() => {
+              languageSelection(swedishSelected);
+            }}
+          />
+        </View>
+      );
+    }
+    if (language.id === "fr") {
+      return (
+        <View style={{ gap: 5 }}>
+          <Button
+            title="English"
+            onPress={() => {
+              languageSelection(englishSelected);
+            }}
+          />
+          <Button
+            title="Français"
+            color={"orange"}
+            onPress={() => {
+              languageSelection(frenchSelected);
+            }}
+          />
+          <Button
+            title="Svenska"
+            onPress={() => {
+              languageSelection(swedishSelected);
+            }}
+          />
+        </View>
+      );
+    }
+  };
+
+  const TempUnitButons = () => {
+    if (tempUnit === "celsius") {
+      return (
+        <View style={{ gap: 5 }}>
+          <Button
+            title="Celsius"
+            color={"orange"}
+            onPress={() => saveTempUnit("celsius")}
+          />
+          <Button title="Farenheit" onPress={() => saveTempUnit("farenheit")} />
+        </View>
+      );
+    }
+
+    if (tempUnit === "farenheit") {
+      return (
+        <View style={{ gap: 5 }}>
+          <Button title="Celsius" onPress={() => saveTempUnit("celsius")} />
+          <Button
+            title="Farenheit"
+            color={"orange"}
+            onPress={() => saveTempUnit("farenheit")}
+          />
+        </View>
+      );
+    }
+  };
+
+  return (
+    <View style={styles.main}>
+      <ThemedText style={styles.title}>{language.settings}</ThemedText>
+      <View style={styles.selection}>
+        <ThemedText style={styles.text}>{language.temperatures}: </ThemedText>
+        <TempUnitButons />
+      </View>
+      <View style={styles.selection}>
+        <ThemedText style={styles.text}>{language.language}: </ThemedText>
+        <LanguageButtons />
       </View>
     </View>
   );
