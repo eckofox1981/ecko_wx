721d5e2887ede240b6a130253d9755d056e2b46a
eckofox1981 <eckofox1981@pm.me>
2025-10-21 13:15:59 +0200

    style: add null type to currentWeather and changed default value to'null'. Error handling / load information implemented in CurrentWeather function.

--- store/weatherStore.ts
+++ store/weatherStore.ts
@@ -2,31 +2,12 @@ import { CurrentWeather, ThreeHoursForeCast } from "@/models/weather";
 import { create, StateCreator } from "zustand";
 
 interface CurrentWeatherStore {
-  currentWeather: CurrentWeather;
+  currentWeather: CurrentWeather | null;
   setCurrentWeather: (newWeather: CurrentWeather) => void;
 }
 
 const currentWeatherStore: StateCreator<CurrentWeatherStore> = (set) => ({
-  currentWeather: {
-    weatherMain: "Clouds",
-    weatherDescription: "broken clouds",
-    weatherIcon: "04d",
-    mainTemp: 288.65,
-    mainFeels_like: 287.64,
-    mainTemp_min: 287.23,
-    mainTemp_max: 289.4,
-    mainPressure: 1011,
-    mainHumidity: 53,
-    mainSea_level: 1011,
-    visibility: 10000,
-    windSpeed: 9.77,
-    windDeg: 270,
-    windGust: 0,
-    cloudsAll: 75,
-    dt: new Date(1759593792 * 1000),
-    sysSunrise: new Date(1759554770 * 1000),
-    sysSunset: new Date(1759595490 * 1000),
-  },
+  currentWeather: null,
   setCurrentWeather: (newWeather: CurrentWeather) =>
     set({ currentWeather: newWeather }),
 });
